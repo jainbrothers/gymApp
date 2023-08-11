193dff83ee654844ae0bbd839acd76f2c073f39f
Ajit Jain <ajit4mail@gmail.com>
2023-08-11 08:56:45 +0530

    Added propertyName to user to store field in serpentine case in firestore.

--- app/src/main/java/com/example/gymapp/data/repository/FirebaseUserRepository.kt
+++ app/src/main/java/com/example/gymapp/data/repository/FirebaseUserRepository.kt
@@ -11,10 +11,6 @@ private const val TAG: String = "FirebaseUserRepository"
 
 class FirebaseUserRepository @Inject constructor(private val database: FirebaseFirestore): UserRepository {
     override suspend fun create(user: User) {
-        val user = hashMapOf(
-            "mobile_number" to user.mobileNumber,
-            "user_id" to user.userId
-        )
         database.collection("user")
             .add(user)
             .addOnSuccessListener { documentReference ->
--- app/src/main/java/com/example/gymapp/model/User.kt
+++ app/src/main/java/com/example/gymapp/model/User.kt
@@ -1,6 +1,9 @@
 package com.example.gymapp.model
 
+import com.google.firebase.firestore.PropertyName
 data class User(
+    @get:PropertyName("user_id")
     val userId: String,
+    @get:PropertyName("mobile_number")
     val mobileNumber: String
 )
