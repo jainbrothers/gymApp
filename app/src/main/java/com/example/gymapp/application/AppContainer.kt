4cbc19f36e287a0eac8b231b44aa781179f5639d
“r0j043y” <rahul.jain3@walmart.com>
2023-09-02 13:38:14 +0530

    Following functionality added: 1. free text search using Algolia. 2. redirection from gym details page to session booking

--- app/src/main/java/com/example/gymapp/application/AppContainer.kt
+++ app/src/main/java/com/example/gymapp/application/AppContainer.kt
@@ -20,7 +20,11 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.ViewModel
+import com.algolia.search.client.ClientSearch
+import com.algolia.search.client.Index
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.IndexName
 import com.example.gymapp.MainActivity
 import com.example.gymapp.data.repository.user.FirebaseUserRepository
 import com.example.gymapp.data.repository.gym.GymRepository
@@ -41,7 +45,6 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
@@ -86,6 +89,16 @@ object AppModule {
     @Singleton
     @Provides
     fun provideMainActivity(): MainActivity = MainActivity.getInstance() as MainActivity
+
+    @Singleton
+    @Provides
+    fun provideGymIndex(): Index {
+        val client = ClientSearch(
+            applicationID = ApplicationID("UV7REXJNQJ"),
+            apiKey = APIKey("0b11699168f3d5e74cdc4bd6cdd839ed")
+        )
+        return client.initIndex(indexName = IndexName("Gym"))
+    }
 }
 
 @InstallIn(SingletonComponent::class)
--- app/src/main/java/com/example/gymapp/data/repository/gym/FirebaseGymRepository.kt
+++ app/src/main/java/com/example/gymapp/data/repository/gym/FirebaseGymRepository.kt
@@ -16,26 +16,61 @@
 
 package com.example.gymapp.data.repository.gym
 
+import com.algolia.search.client.ClientSearch
+import com.algolia.search.client.Index
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
 import com.example.gymapp.data.repository.GYM_TABLE_NAME
 import com.example.gymapp.model.Gym
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.dataObjects
 import kotlinx.coroutines.flow.Flow
-
+import com.algolia.search.model.IndexName
+import com.algolia.search.model.ObjectID
+import com.algolia.search.model.indexing.Indexable
+import com.algolia.search.model.search.Query
+import com.algolia.search.helper.deserialize
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 
-class FirebaseGymRepository @Inject constructor(private val database: FirebaseFirestore) :
+class FirebaseGymRepository @Inject constructor(private val database: FirebaseFirestore,
+                                                private val index: Index
+) :
     GymRepository {
     override val gyms: Flow<List<Gym>>
         get() = database.collection(GYM_TABLE_NAME).dataObjects()
-    override suspend fun getGymListBySearch(search: String): Flow<List<Gym>> {
-        return if (search != "") {
-            database.collection(GYM_TABLE_NAME).whereGreaterThanOrEqualTo("type", search).dataObjects()
-        } else {
-            gyms
-        }
-    }
+
     override suspend fun getGymById(docId: String): Flow<Gym?> =
         database.collection(GYM_TABLE_NAME).document(docId).dataObjects()
+
+    override suspend fun getGymListBySearch(search: String): Flow<List<Gym>> {
+        // Connect and authenticate with your Algolia app
+
+        val response = index.run {
+            search(Query(search))
+        }
+
+        val results: List<Gym> = response.hits.deserialize(Gym.serializer())
+
+//        return gyms
+        return flow {
+            emit(results.toList())
+        }
+
+    // Create a new index and add a record (using the `Record` class)
+//        val index = client.initIndex(indexName = IndexName("test_index"))
+//        val record = Record("test_record", ObjectID("1"))
+//
+//        index.run {
+//            saveObject(Record.serializer(), record).wait()
+//        }
+//
+//        // Search the index and print the results
+//        val response = index.run {
+//            search(Query("test_record"))
+//        }
+//        val results: List<Record> = response.hits.deserialize(Record.serializer())
+//        println(results[0])
+    }
 }
