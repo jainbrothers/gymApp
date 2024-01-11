0ce2d5e2d85654a38d17a1e44de2d381347c073d
Ajit Jain <ajit4mail@gmail.com>
2024-01-11 13:40:05 +0530

    Using constant variable for book session table name instead of hard coding.

--- app/src/main/java/com/example/gymapp/data/repository/session/FirebaseBookedSessionRepository.kt
+++ app/src/main/java/com/example/gymapp/data/repository/session/FirebaseBookedSessionRepository.kt
@@ -1,5 +1,6 @@
 package com.example.gymapp.data.repository.session
 
+import com.example.gymapp.data.repository.BOOKED_SESSION_TABLE_NAME
 import com.example.gymapp.data.repository.GYM_TABLE_NAME
 import com.example.gymapp.model.session.BookedSessionDetail
 import com.example.gymapp.ui.screen.enumeration.ErrorCode
@@ -11,7 +12,7 @@ import javax.inject.Inject
 class FirebaseBookedSessionRepository @Inject constructor(private val database: FirebaseFirestore): BookedSessionRepository {
     override suspend fun create(bookedSessionDetail: BookedSessionDetail, docId: String) {
         try {
-            database.collection("booked_session").document(docId).set(bookedSessionDetail)
+            database.collection(BOOKED_SESSION_TABLE_NAME).document(docId).set(bookedSessionDetail)
         } catch (firebaseException: FirebaseException) {
             throw ErrorCode.ThirdPartyServiceException("Firebase error occurred while adding booked session detail: ${bookedSessionDetail}", firebaseException)
         } catch (exception: Exception) {
