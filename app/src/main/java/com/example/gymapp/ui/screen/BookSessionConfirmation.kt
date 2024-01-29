b5878b925ef601be861ca08e2d2ba83a3b50575a
jainbrothers <66871876+jainbrothers@users.noreply.github.com>
2024-01-29 12:29:16 +0530

    Booking confirmation snackbar (#20)
    * Added Snackbar message on user confirmation of session booking.

--- app/src/main/java/com/example/gymapp/ui/screen/BookSessionConfirmation.kt
+++ app/src/main/java/com/example/gymapp/ui/screen/BookSessionConfirmation.kt
@@ -5,18 +5,28 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.gymapp.R
 import com.example.gymapp.ui.screen.enumeration.ErrorCode
 import com.example.gymapp.ui.screen.viewmodel.BookSessionConfirmationViewModel
 import com.example.gymapp.ui.screen.viewmodel.state.BookSessionConfirmationUiState
+import kotlinx.coroutines.launch
 import java.util.Date
 
 @Composable
@@ -37,7 +47,6 @@ fun BookSessionConfirmation(
         }
     }
 }
-
 @Composable
 fun RenderErrorMessage(errorCode: ErrorCode, modifier: Modifier = Modifier) {
     Column(
@@ -84,29 +93,44 @@ fun RenderBookSessionDetail(uiState: BookSessionConfirmationUiState, modifier: M
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ConfirmButton(
     bookSessionConfirmationViewModel: BookSessionConfirmationViewModel,
     onClickConfirmButton: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column (
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    )
-    {
-        Button(
-            onClick = {
-                bookSessionConfirmationViewModel.createBookedSessionDetail()
-                onClickConfirmButton()
+    val snackbarHostState = remember { SnackbarHostState()}
+    val confirmationMessage = stringResource(R.string.SESSION_BOOKING_CONFIRMATION_TEXT)
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = {SnackbarHost(hostState = snackbarHostState)}
+    ) { padding ->
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        )
+        {
+            Button(
+                onClick = {
+                    scope.launch {
+                        bookSessionConfirmationViewModel.createBookedSessionDetail()
+                        snackbarHostState.showSnackbar(
+                            message = confirmationMessage,
+                            duration = SnackbarDuration.Short
+                        )
+                        onClickConfirmButton()
+                    }
+
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.CONFIRM_BUTTON_TEXT),
+                    fontWeight = FontWeight.Bold
+                )
             }
-        ) {
-            Text(
-                text = "Confirm",
-                fontWeight = FontWeight.Bold
-            )
         }
     }
 }
